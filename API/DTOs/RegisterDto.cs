e617ec4003dce98a8d245a168377d8f500963a86
Pratm21 <pmpratikmore1@gmail.com>
2024-12-22 00:22:12 +0100

    end of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,9 +7,10 @@ public class RegisterDto
 {
     [Required]
     [MaxLength(100)]     // both required annotation in [] and required keyword does same thing
-    public required string Username { get; set; }
+    public string Username { get; set; } = String.Empty;
 
     [Required]
-    public required string Password { get; set; }
+    [StringLength(8, MinimumLength = 4)]
+    public string Password { get; set; } = String.Empty;
 
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,12 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 ApplicationServiceExtensions.AddApplicationServices(builder.Services, builder.Configuration);
 IdentityServiceExtension.AddIdentityServices(builder.Services, builder.Configuration);
 var app = builder.Build();
-
+app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseAuthentication();
